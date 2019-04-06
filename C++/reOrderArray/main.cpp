7e7bcb5f6244b0057cb434e5e03a61c18e2c2e33
ArafatBUPT <arafat@bupt.edu.cn>
2019-04-06 20:01:52 +0800

    reorderarray

--- C++/reOrderArray/main.cpp
+++ C++/reOrderArray/main.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
+#include "queue"
+
+using namespace std;
+class Solution {
+public:
+    void reOrderArray(vector<int> &array) {
+
+        if (array.size()==0||array.size()==1)
+            return;
+
+        queue<int> even;
+        vector<int>::iterator cur;
+        for (cur=array.begin();cur!=array.end();) {
+            if (!(*cur%2)){
+                even.push(*cur);
+                array.erase(cur);
+            }
+            else
+                cur++;
+        }
+
+        while (!even.empty()){
+            array.push_back(even.front());
+            even.pop();
+        }
+    }
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
