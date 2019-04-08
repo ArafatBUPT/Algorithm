8adbf72f7c4b1f20a4e1eb729fcdc1f3f6a60e4f
ArafatBUPT <arafat@bupt.edu.cn>
2019-04-08 15:15:45 +0800

    4/8

--- C++/HasSubtree/main.cpp
+++ C++/HasSubtree/main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+
+
+class Solution {
+private:
+    bool DoseTreeAhasTreeB(TreeNode* pRootA,TreeNode* pRootB){
+
+
+        if(pRootB==NULL)
+            return true;
+
+        if(pRootA==NULL)
+            return false;
+
+        if(pRootA->val!=pRootB->val)
+            return false;
+
+        return DoseTreeAhasTreeB(pRootA->left,pRootB->left)&&DoseTreeAhasTreeB(pRootA->right,pRootB->right);
+
+
+    }
+public:
+    bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2) {
+        //flag记录状态 初始false
+        bool flag=false;
+
+        if(pRoot1!=NULL && pRoot2!=NULL) {
+            flag = DoseTreeAhasTreeB(pRoot1, pRoot2);
+            //如果当前值不相等 返回false
+
+            //找到A和B相同的第一元素
+            if (!flag)
+                flag = HasSubtree(pRoot1->left, pRoot2);
+            if (!flag)
+                flag = HasSubtree(pRoot1->right, pRoot2);
+        }
+        return flag;
+
+    }
+};
---
+++ C++/Mirror/cmake-build-debug/CMakeFiles/Mirror.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mirror.dir/main.cpp.obj"
+  "Mirror.pdb"
+  "Mirror.exe"
+  "Mirror.exe.manifest"
+  "libMirror.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mirror.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
