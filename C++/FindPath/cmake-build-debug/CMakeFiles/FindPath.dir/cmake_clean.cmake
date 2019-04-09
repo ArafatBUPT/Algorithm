4dd189853a7547ad645b0e59c1d734021bedae6a
ArafatBUPT <arafat@bupt.edu.cn>
2019-04-09 12:37:52 +0800

    4/9

---
+++ C++/FindPath/cmake-build-debug/CMakeFiles/FindPath.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FindPath.dir/main.cpp.obj"
+  "FindPath.pdb"
+  "FindPath.exe"
+  "FindPath.exe.manifest"
+  "libFindPath.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FindPath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/FindPath/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "vector"
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+class Solution {
+private:
+    void find(TreeNode* node,int num,vector<int> &vec,vector<vector<int>> &res){
+        if(node==NULL)
+            return;
+
+        //如果是子路径 而且是最后子节点 存储到res
+        if (node->val==num&&node->right==NULL&&node->right==NULL) {
+            vec.push_back(node->val);
+            res.push_back(vec);
+            vec.pop_back();
+            return;
+        }
+        if(node->val<num)
+            //每次递归 减去该节点的值 再向下传递 
+            vec.push_back(node->val);
+
+        find(node->left,num-node->val,vec,res);
+        find(node->right,num-node->val,vec,res);
+        vec.pop_back();
+
+    }
+public:
+    vector<vector<int>> FindPath(TreeNode* root,int expectNumber) {
+
+        vector<vector<int>> res;
+        vector<int> record;
+        //record存每个子路径 res存总共路径
+        find(root,expectNumber,record,res);
+        return res;
+    }
+};
+
