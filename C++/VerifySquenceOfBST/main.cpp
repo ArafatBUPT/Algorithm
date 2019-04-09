978f076715e72691f0c7b72483e6af6ad3679865
ArafatBUPT <arafat@bupt.edu.cn>
2019-04-09 10:35:43 +0800

    4/9

--- C++/VerifySquenceOfBST/main.cpp
+++ C++/VerifySquenceOfBST/main.cpp
@@ -4,30 +4,26 @@ using namespace std;
 
 
 class Solution {
+private:
+    bool judge(vector<int> arr,int l ,int r){
+        //只剩下一个元素 返回
+        if(l>=r) return true;
+
+        //i指向最后一个比根小的元素
+        int i=r-1;
+        while (i>l && arr[i]>arr[r]) --i;
+
+        //前面的元素比根大 返回false
+        for (int j = i-1; j >=l ; --j) {
+            if (arr[j]>arr[r])
+                return false;
+        }
+        //区间是闭区间[];
+        return judge(arr,l,i) && judge(arr,i+1,r-1);
+    }
 public:
-    bool VerifySquenceOfBST(vector<int> sequence) {
-
-        bool flag= true;
-        int size=sequence.size();
-
-        if (sequence[size-1]<sequence[size-2]) {
-            for (int i = size - 2; i > 0; i -= 2) {
-                if (sequence[i] < sequence[i - 1]) {
-                    flag = false;
-                    break;
-                }
-            }
-            return flag;
-        }
-        else{
-            for (int i = size-2; i >0 ; i-=2) {
-                if (sequence[i]>sequence[i-1]) {
-                    flag = false;
-                    break;
-                }
-            }
-            return flag;
-        }
-
+    bool VerifySquenceOfBST(vector<int> arr) {
+        if(!arr.size()) return false;
+        return judge(arr,0,arr.size()-1);
     }
 };
